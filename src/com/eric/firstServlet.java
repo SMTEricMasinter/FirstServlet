88788a85b3d40230126cca30b8352680cac53af6
ReadingAddict <eric.masinter@gmail.com>
2015-07-20 10:28:14 -0600

    init

---
+++ src/com/eric/firstServlet.java
@@ -0,0 +1,47 @@
+package com.eric;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class firstServlet
+ */
+//@WebServlet("/firstServlet")
+public class firstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public firstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//performTask(request, response);
+		System.out.println("ok here");
+		this.getServletContext().getRequestDispatcher("/WEB-INF/include/main.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	doGet(request, response);
+	}
+	
+
+
+}
